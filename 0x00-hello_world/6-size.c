615f0461151832f759211a30180f4f827a4d8d16
JMI-17 <jmi17.jm@gmail.com>
2023-10-05 14:00:32 -0700

    0x0 Test6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- *
+ * This line defines the main function
+ * This line uses the printf function to print the size of the'char,int,long int,long long int
+ * float'
  * Return: Always 0 (success)
  */
 int main(void)
@@ -11,7 +13,5 @@ printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
-printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 return (0);
 }
