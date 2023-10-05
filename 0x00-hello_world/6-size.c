858519274b6052741e39d6ee09d97dc5fa466390
JMI-17 <jmi17.jm@gmail.com>
2023-10-05 14:41:04 -0700

    0x0 Test6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- * 
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
