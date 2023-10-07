ffc1a429f21744356a1d89211a8c576a633ffb42
JMI-17 <jmi17.jm@gmail.com>
2023-10-07 14:26:37 -0700

    6-print_numberz.c Commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,23 +2,20 @@
 /**
 * main - Entry point of the program
 *
-* Description: This function prints all single-digit numbers of base 10
-* followed by a newline character.
+* Description:
+* This function prints all single-digit numbers of base 10
+* followed by a newline character using only two putchar calls.
 *
 * Return: Always 0 (success)
 */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
+int digit = 0;
+while (digit <= 9)
+{
+putchar('0' + digit);
+digit++;
+}
 putchar('\n');
 return (0);
 }
