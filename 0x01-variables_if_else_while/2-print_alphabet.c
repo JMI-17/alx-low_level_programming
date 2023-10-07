1aa8b15716eeecf7cb6e20fd94d027184d9b9573
JMI-17 <jmi17.jm@gmail.com>
2023-10-06 20:14:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the alphabet in lowercase, followed by a newline.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
