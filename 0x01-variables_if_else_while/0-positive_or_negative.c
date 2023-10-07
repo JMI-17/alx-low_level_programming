a0955e72d5d58f7ec5cb24a38b7b21dc4adf930b
JMI-17 <jmi17.jm@gmail.com>
2023-10-06 18:36:16 -0700

    First Ifelwh commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%d is ", n);  // Print the number
+printf("%d is ", n);
 if (n > 0)
 {
 printf("positive\n");
