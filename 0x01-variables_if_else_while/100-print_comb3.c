3851a1f7d7f7a1bcf640d90e832b20f2e618b859
JMI-17 <jmi17.jm@gmail.com>
2023-10-08 01:54:41 -0700

    100-print_comb3.c commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,17 +16,17 @@ int main(void)
 {
 int firstDigit, secondDigit;
 for (firstDigit = 0; firstDigit <= 8; firstDigit++)
-{       
+{
 for (secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
-{       
-putchar((firstDigit % 10) + '0'); 
-putchar((secondDigit % 10) + '0'); 
+{
+putchar((firstDigit % 10) + '0');
+putchar((secondDigit % 10) + '0');
 if (firstDigit != 8 || secondDigit != 9)
-{       
+{
 putchar(',');
 putchar(' ');
-}       
-}       
+}
+}
 }
 putchar('\n');
 return (0);
