5099662735a7f496985411322f6996c6d3c34bb0
JMI-17 <jmi17.jm@gmail.com>
2023-10-10 04:50:57 -0700

    0-putchar.c T1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+* main - Entry point of the program
+*
+* Description: This program prints the word "_putchar" followed by a newline
+* character to the standard output.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
     _putchar('_');
