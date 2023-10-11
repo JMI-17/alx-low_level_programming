5ac653bebe2e000f320e53f87cd2c06ed3f9627b
JMI-17 <jmi17.jm@gmail.com>
2023-10-10 19:25:45 -0700

    1-alphabet.c Test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-*print_alphabet
-*- Prints the lowercase alphabet followed by a new line.
+* print_alphabet - Prints the lowercase alphabet followed by a new line.
 */
 void print_alphabet(void)
 {
