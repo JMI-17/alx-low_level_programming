ca2d40442c9a70b7745eac4b63db47b7c078bfff
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 14:42:53 -0700

    0x04 T9 FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+
 /**
-* main - Entry point for the Fizz-Buzz program
-*
-* Description:
-* This program prints numbers from 1 to 100, applying the
-* Fizz-Buzz rules. It prints "Fizz" for multiples of 3,
-* "Buzz" for multiples of 5, and "FizzBuzz" for multiples of both, otherwise,
-* it prints the number.
-* Return: 0 (Success)
-*/
-int main(void) {
-for (int i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
-printf("FizzBuzz ");
-} else if (i % 3 == 0) {
-printf("Fizz ");
-} else if (i % 5 == 0) {
-printf("Buzz ");
-} else {
-printf("%d ", i);
-}
-}
-printf("\n");
-return (0);
+ * main - Entry point for the Fizz-Buzz program
+ *
+ * Description: Prints numbers from 1 to 100 with conditions:
+ * - If divisible by 3, print "Fizz."
+ * - If divisible by 5, print "Buzz."
+ * - If divisible by both 3 and 5, print "FizzBuzz."
+ * - Otherwise, print the number itself.
+ *
+ * Return: 0 (Success)
+ */
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz ");
+        } else if (i % 3 == 0) {
+            printf("Fizz ");
+        } else if (i % 5 == 0) {
+            printf("Buzz ");
+        } else {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
 }
