25f2b19ddb664d1074f88c5bc4ee2dabb4479eed
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 06:26:48 -0700

    3-print_remaining_days.c T3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,23 +2,22 @@
 #include "main.h"
 
 /**
-* is_leap_year - checks if a year is a leap year
-* @year: the year to check
-* Return: 1 if leap year, 0 otherwise
-*/
+ * is_leap_year - checks if a year is a leap year
+ * @year: the year to check
+ * Return: 1 if leap year, 0 otherwise
+ *
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 int is_leap_year(int year)
 {
     return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
 
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
 void print_remaining_days(int month, int day, int year)
 {
     int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
