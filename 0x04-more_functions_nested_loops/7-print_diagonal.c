83694c99dfe37515b6b192045a9333ebc4815e45
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 13:55:24 -0700

    0x04 T7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,31 +1,27 @@
 #include "main.h"
 /**
- * print_diagonal - Draw a diagonal line of backslashes
- * @n: The number of times the character '\' should be printed
- */
+* print_diagonal - Draw a diagonal line of backslashes
+* @n: The number of times the character '\'
+* should be printed
+*/
 void print_diagonal(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int spaces = 0;
-
-        while (n > 0)
-        {
-            int i;
-
-            for (i = 0; i < spaces; i++)
-                _putchar(' ');
-
-            _putchar('\\');
-            _putchar('\n');
-
-            spaces++;
-            n--;
-        }
-    }
+if (n <= 0)
+{
+_putchar('\n');
 }
-
+else
+{
+int spaces = 0;
+while (n > 0)
+{
+int i;
+for (i = 0; i < spaces; i++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+spaces++;
+n--;
+}
+}
+}
