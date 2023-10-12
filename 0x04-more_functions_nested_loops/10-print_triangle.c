063d6060283ed73597f58f558ca7333390e39c90
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 14:52:56 -0700

    0x04 T10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_triangle - Print a right-angled triangle of '#' characters
+* @size: The size of the triangle
+*/
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
