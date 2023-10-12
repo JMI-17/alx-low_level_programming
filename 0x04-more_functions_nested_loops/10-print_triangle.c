e41604b0d1652d1be667c3791bf72e12cccbb6c7
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 15:46:27 -0700

    0x04 T10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,10 +12,13 @@ _putchar('\n');
 else
 {
 int i, j;
-for (i = 1; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 1; j <= i; j++)
+for (j = 0; j < size; j++)
 {
+if (j < size - i - 1)
+_putchar(' ');
+else
 _putchar('#');
 }
 _putchar('\n');
