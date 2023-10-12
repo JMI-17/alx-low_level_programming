58e1583579be59fbf4bbfabecffce3f1b46c03ec
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 16:46:51 -0700

    0x04 T11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,29 +2,40 @@
 #include <math.h>
 /**
 * findLargestPrimeFactor - Find the largest prime factor of a number.
+*
 * @n: The number for which the largest prime factor is to be found.
 *
 * Return: The largest prime factor of the number.
 */
-long long findLargestPrimeFactor(long long n) {
+long long findLargestPrimeFactor(long long n)
+{
 long long maxPrime = -1;
-/* Remove all the 2s from n */
-while (n % 2 == 0) {
+while (n % 2 == 0)
+{
 maxPrime = 2;
 n /= 2;
 }
-for (long long i = 3; i <= sqrt(n); i += 2) {
-while (n % i == 0) {
+for (long long i = 3; i <= sqrt(n); i += 2)
+{
+while (n % i == 0)
+{
 maxPrime = i;
 n /= i;
 }
 }
-if (n > 2) {
+if (n > 2)
+{
 maxPrime = n;
 }
-return maxPrime;
+return (maxPrime);
 }
-int main() {
+/**
+* main - Entry point of the program
+*
+* Return: 0 on success.
+*/
+int main(void)
+{
 long long number = 612852475143;
 long long largestPrimeFactor = findLargestPrimeFactor(number);
 printf("%lld\n", largestPrimeFactor);
