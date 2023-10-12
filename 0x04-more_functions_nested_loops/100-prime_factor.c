361e787c493e07316e98ec411218da218c466aa0
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 14:59:54 -0700

    0x04 T11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,32 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 /**
-* print_triangle - Print a right-angled triangle of '#' characters
-* @size: The size of the triangle
+* findLargestPrimeFactor - Find the largest prime factor of a number.
+* @n: The number for which the largest prime factor is to be found.
+*
+* Return: The largest prime factor of the number.
 */
-void print_triangle(int size)
-{
-if (size <= 0)
-{
-_putchar('\n');
+long long findLargestPrimeFactor(long long n) {
+long long maxPrime = -1;
+/* Remove all the 2s from n */
+while (n % 2 == 0) {
+maxPrime = 2;
+n /= 2;
 }
-else
-{
-int i, j;
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
+for (long long i = 3; i <= sqrt(n); i += 2) {
+while (n % i == 0) {
+maxPrime = i;
+n /= i;
 }
 }
+if (n > 2) {
+maxPrime = n;
+}
+return maxPrime;
+}
+int main() {
+long long number = 612852475143;
+long long largestPrimeFactor = findLargestPrimeFactor(number);
+printf("%lld\n", largestPrimeFactor);
+return (0);
 }
