0d5e3de15c8930e10e8bfb264bc848117c9c474a
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 13:23:51 -0700

    0x04 T6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_line - Print a straight line of underscores
+* print_line - Draw a straight line of underscores
 * @n: The number of times the character '_' should be printed
 */
 void print_line(int n)
@@ -11,9 +11,10 @@ _putchar('\n');
 }
 else
 {
-for (int i = 0; i < n; i++)
+while (n > 0)
 {
 _putchar('_');
+n--;
 }
 _putchar('\n');
 }
