1e353c6bf65f1aaca84018359bc48a6df2f46b6b
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 07:06:44 -0700

    3-print_remaining_days.c T3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,50 +22,7 @@
 int is_leap_year(int year)
 {
     return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-}
-
-int convert_day(int month, int day)
-{
-    switch (month)
-    {
-        case 2:
-            day = 31 + day;
-            break;
-        case 3:
-            day = 59 + day;
-            break;
-        case 4:
-            day = 90 + day;
-            break;
-        case 5:
-            day = 120 + day;
-            break;
-        case 6:
-            day = 151 + day;
-            break;
-        case 7:
-            day = 181 + day;
-            break;
-        case 8:
-            day = 212 + day;
-            break;
-        case 9:
-            day = 243 + day;
-            break;
-        case 10:
-            day = 273 + day;
-            break;
-        case 11:
-            day = 304 + day;
-            break;
-        case 12:
-            day = 334 + day;
-            break;
-        default:
-            break;
-    }
-    return day;
-}
+} 
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -74,7 +31,7 @@ void print_remaining_days(int month, int day, int year)
 
     if (is_leap)
     {
-        days_in_month[2] = 29; // Update February days for leap year
+        days_in_month[2] = 29;
     }
 
     if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
@@ -92,7 +49,7 @@ void print_remaining_days(int month, int day, int year)
 
     if (is_leap && month > 2)
     {
-        day_of_year++; // Adjust for the extra day in leap year
+        day_of_year++;
     }
 
     printf("Day of the year: %d\n", day_of_year);
