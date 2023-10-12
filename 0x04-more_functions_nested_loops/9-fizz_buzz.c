d439e92e19eaa9d730f140441b6b8b35b9b49d7b
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 14:45:52 -0700

    0x04 T9 FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point for the Fizz-Buzz program
- *
- * Description: Prints numbers from 1 to 100 with conditions:
- * - If divisible by 3, print "Fizz."
- * - If divisible by 5, print "Buzz."
- * - If divisible by both 3 and 5, print "FizzBuzz."
- * - Otherwise, print the number itself.
- *
- * Return: 0 (Success)
- */
 int main() {
     for (int i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
