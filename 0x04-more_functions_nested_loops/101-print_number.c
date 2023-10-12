a4b53492d6ef507054dc070f7a0a8c287e147044
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 15:57:59 -0700

    0x04 T12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,18 @@
 #include "main.h"
+#include <limits.h>
 /**
- * print_number - Print an integer
- * @n: The integer to be printed
- */
+* print_number - Print an integer
+* @n: The integer to be printed
+*/
 void print_number(int n)
+{
+if (n == INT_MIN)
+{
+_putchar('-');
+print_number(n / -10);
+_putchar('0' - (n % 10));
+}
+else
 {
 if (n < 0)
 {
@@ -15,4 +24,5 @@ if (n / 10 != 0)
 print_number(n / 10);
 }
 _putchar(n % 10 + '0');
+}
 }
