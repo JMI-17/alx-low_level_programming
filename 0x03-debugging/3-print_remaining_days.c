17b49cce0597b0ac0985f57d39332e8ce8c6eab4
JMI-17 <jmi17.jm@gmail.com>
2023-10-12 18:10:43 -0700

    0x03 T3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,45 +4,31 @@
 * print_remaining_days - takes a date and prints how many days are left
 * in the year, taking leap years into account
 * @month: month in number format
-* @day: day of month
+* @day: day of the month
 * @year: year
 * Return: void
 */
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 {
 if (month >= 2 && day >= 60)
 {
 day++;
 }
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
-int day_of_year = 0;
-int days_in_month[] = {
-0, 31, 28, 31, 30, 31, 30, 31,
-31, 30, 31, 30, 31
-};
-if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+else
 {
-days_in_month[2] = 29;
-}
-if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
-for (int i = 1; i < month; i++)
+else
 {
-day_of_year += days_in_month[i];
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
-day_of_year += day;
-if (month > 2)
-{
-day_of_year++;
 }
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n",
-(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-? 366 - day_of_year
-: 365 - day_of_year);
 }
