cff41ca2687dcfefffd83d98bdef097c7265b4c1
JMI-17 <jmi17.jm@gmail.com>
2023-10-13 02:34:03 -0700

    0x03 T1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
+* 
 * Return: 0*
+*
 */
 int main(void)
 {
@@ -11,8 +13,11 @@ i = 0;
 /*
 * while (i < 10)
 * (
+* 
 * putchar(i);
+* 
 * }
+*
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
