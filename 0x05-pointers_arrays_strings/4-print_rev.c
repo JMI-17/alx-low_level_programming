e03e4d0eb4d10acd7659ddaa2b849528ddd6ee07
JMI-17 <jmi17.jm@gmail.com>
2023-10-16 10:09:17 -0700

    0x05 T3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* print_rev - Prints a string in reverse, followed by a new line.
+* @s: The string to be printed in reverse.
+*/
+void print_rev(char *s) {
+int i = 0;
+while (s[i] != '\0') {
+i++;
+}
+while (i > 0) {
+i--;
+putchar(s[i]);
+}
+putchar('\n');
+}
+int main() {
+char str[] = "Hello, World!";
+print_rev(str);
+return (0);
+}
