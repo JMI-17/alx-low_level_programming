28fc8ae4d19e02407870a2a7e6aa5be42eac5504
JMI-17 <jmi17.jm@gmail.com>
2023-10-17 02:38:33 -0700

    0x02 T6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,13 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-int n, abs_value;
-n = -98;
-abs_value = _abs(n);
-if (abs_value < 0)
-_putchar('-');    
-print_number(abs_value);
-_putchar('\n');
-return (0);
-}
-/**
- * print_number - Print an integer using _putchar
- * @n: The integer to print
- */
-void print_number(int n)
+* _abs - Compute the absolute value of an integer
+* @n: The integer to find the absolute value of
+*
+* Return: The absolute value of n
+*/
+int _abs(int n)
 {
 if (n < 0)
-n = -n;
-if (n / 10 != 0)
-print_number(n / 10);
-_putchar((n % 10) + '0');
+return -n;
+return n;
 }
