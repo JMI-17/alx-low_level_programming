cc3cd93ebe35aab9b0798807a9015d885483b288
JMI-17 <jmi17.jm@gmail.com>
2023-10-18 03:33:08 -0700

    0x02 T11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+/**
+* main -
+* Entry point for computing the sum of multiples of 3 or 5 below 1024
+* Description:
+* This program calculates and prints the sum of all the multiples of
+* 3 or 5
+* The sum of multiples is then displayed.
+*
+* Return: Always 0 (success)
+*/
 int main()
 {
 int sum = 0;
 int limit = 1024;
-for (int i = 0; i < limit; i++)
+for (i = 0; i < limit; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
