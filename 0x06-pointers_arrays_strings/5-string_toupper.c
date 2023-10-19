33dc309b75af98f6302486a42eab6f98a1bc5553
JMI-17 <jmi17.jm@gmail.com>
2023-10-19 14:18:07 -0700

    0x06 T0-12

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,11 @@
+#include "main.h"
 /**
-* string_toupper-
-* Convert lowercase letters to uppercase in a string.
+* string_toupper- Convert lowercase letters to uppercase in a string.
+*
 * @str: The input string to be modified.
 *
 * Return: A pointer to the modified string.
+*
 */
 char *string_toupper(char *str)
 {
