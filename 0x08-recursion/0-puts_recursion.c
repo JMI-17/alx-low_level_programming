ca56fa0ee699fc61040a449ebddbe34acf6eea8d
JMI-17 <jmi17.jm@gmail.com>
2023-10-25 09:49:05 -0700

    0x08 T0-08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -17,9 +17,3 @@ return;
 putchar(*s);
 _puts_recursion(s + 1);
 }
-int main(void)
-{
-char str[] = "Puts with recursion";
-_puts_recursion(str);
-return (0);
-}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,4 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
-
 void _puts_recursion(char *s);
-
 #endif /* MY_HEADER_H */
