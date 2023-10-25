68413cdb112a6179921b7fef9d172781576af53f
JMI-17 <jmi17.jm@gmail.com>
2023-10-25 14:42:10 -0700

    0x08 T0-08

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,14 +23,6 @@ else
 return (calculate_sqrt(n, x + 1));
 }
 }
-/** Return: The natural square root of n.
-* _sqrt_recursion - Calculate the natural square
-* root of a number using recursion.
-* @n: The number for which to calculate the square root.
-*
-* Return: The natural square root of n. If n does not have a
-* natural square root, return -1.
-*/
 int _sqrt_recursion(int n)
 {
 if (n < 0)
