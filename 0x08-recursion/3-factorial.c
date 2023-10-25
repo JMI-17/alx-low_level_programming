197aca202107f693f0584362b099599788c61539
JMI-17 <jmi17.jm@gmail.com>
2023-10-25 10:58:33 -0700

    0x08 T0-08

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* factorial - Calculate the factorial of a number.
+* @n: The number for which to calculate the factorial.
+*
+* Return: The factorial of n. 
+* If n is negative, return -1 to indicate an error.
+* Factorial of 0 is 1.
+*/
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+int result = 1;
+int i;
+for (i = 1; i <= n; i++)
+{
+result *= i;
+}
+return (result);
+}
+}
