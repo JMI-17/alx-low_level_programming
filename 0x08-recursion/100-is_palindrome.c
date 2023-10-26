1b2751e131edffe117e8b300c81247ec28911446
JMI-17 <jmi17.jm@gmail.com>
2023-10-26 07:39:42 -0700

    0x08 T0-08

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* is_palindrome - Checks if a string is a palindrome.
+* @s: The input string to check.
+*
+* Return: 1 if the string is a palindrome, 0 if not.
+*/
+int is_palindrome(char *s)
+{
+int left = 0;
+int right = strlen(s) - 1;
+while (left < right)
+{
+if (s[left] != s[right])
+{
+return (0);
+}
+left++;
+right--;
+}
+return (1);
+}
