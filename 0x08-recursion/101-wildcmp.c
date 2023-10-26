f967479d47f429572534deb3c5b03f2efd08678c
JMI-17 <jmi17.jm@gmail.com>
2023-10-26 08:40:56 -0700

    0x08 T0-08

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+int is_palindrome_recursive(char *s, int left, int right)
+/**
+ * wildcmp - Compare two strings with wildcard support.
+ * @s1: The first string to compare.
+ * @s2: The second string with wildcards.
+ *
+ * Return: 1 if the strings can be considered identical
+ * 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && (*s2 == '\0' || *s2 == '*'))
+return (1);
+if (*s2 == '*')
+{
+if (wildcmp(s1, s2 + 1))
+return (1);
+if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+return (1);
+}
+if (*s1 == *s2 || *s2 == '?')
+return wildcmp(s1 + 1, s2 + 1);
+return (0);
+}
