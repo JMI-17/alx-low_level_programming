9ff2345b780b5b117fa49d45cd4e50682e955ee9
JMI-17 <jmi17.jm@gmail.com>
2023-10-27 09:23:49 -0700

    OXOA T0-T05

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
 *
 * Return: Always 0.
 */
-int main(int argc, char *argv[]) {
-if (argc > 0 && argv[0] != NULL) {
+int main(int argc, char *argv[])
+{
+if (argc > 0 && argv[0] != NULL)
+{
 printf("%s\n", argv[0]);
 }
 return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,5 @@
 #ifndef PRINT_NAME_H
 #define PRINT_NAME_H
+int _putchar(char c);
 int main(int argc, char *argv[]);
 #endif /* PRINT_NAME_H */
