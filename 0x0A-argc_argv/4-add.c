ce85a01d1e795d328784a2a4041557039778a005
JMI-17 <jmi17.jm@gmail.com>
2023-10-28 08:51:35 -0700

    0x0A T0-05

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* is_positive_number - Check if a string represents a positive number
+* @str: The string to check
+*
+* Return: 1 if it's a positive number, 0 otherwise
+*/
 int is_positive_number(char *str)
 {
 if (*str == '\0')
@@ -17,6 +23,13 @@ str++;
 }
 return (1);
 }
+/**
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array containing the command-line arguments
+*
+* Return: 0 on success, 1 on error
+*/
 int main(int argc, char *argv[])
 {
 int sum = 0;
