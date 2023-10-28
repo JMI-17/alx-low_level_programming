bad3573274202340899343b011a0af5c76f9f550
JMI-17 <jmi17.jm@gmail.com>
2023-10-28 09:10:39 -0700

    0x0A T0-05

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,76 +1,39 @@
 #include "main.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
-* main - Entry point of the program.
-* @argc: The number of command-line arguments.
-* @argv: An array of command-line argument strings.
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array containing the command-line arguments
 *
-* Return: 0 on success, 1 on error.
+* Return: (0) on success, (1) on error
 */
 int main(int argc, char *argv[])
 {
-if (argc != 2)
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-return (1);
-}
-int cents = 0;
-char *arg = argv[1];
-int i = 0;
-if (arg[0] == '-')
-{
-_putchar('0');
-_putchar('\n');
-return (0);
-}
-while (arg[i] != '\0')
-{
-if (arg[i] >= '0' && arg[i] <= '9')
-{
-cents = cents * 10 + (arg[i] - '0');
-}
-else
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-return (1);
-}
-i++;
-}
+int cents;
 int coins[] = {25, 10, 5, 2, 1};
 int num_coins = sizeof(coins) / sizeof(coins[0]);
-int count = 0;
-i = 0;
-while (cents > 0 && i < num_coins)
+int min_coins = 0;
+int i;
+if (argc != 2)
 {
-if (cents >= coins[i])
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
 {
-int num = cents / coins[i];
-count += num;
-cents -= num * coins[i];
+printf("0\n");
+return (0);
 }
-i++;
-}
-if (cents == 0)
+for (i = 0; i < num_coins; i++)
 {
-char c;
-while (count > 0)
+while (cents >= coins[i])
 {
-c = (count % 10) + '0';
-_putchar(c);
-count /= 10;
+cents -= coins[i];
+min_coins++;
 }
-_putchar('\n');
 }
+printf("%d\n", min_coins);
 return (0);
 }
