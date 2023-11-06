35cc822fd9f21ef2e9fcbf5d90ddb96b0d39f98f
JMI-17 <jmi17.jm@gmail.com>
2023-11-06 08:13:03 -0800

    OXOD T0-T05

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef MATH_MACROS_H
+#define MATH_MACROS_H
+#define SUM(x, y) ((x) + (y))
+#endif /* MATH_MACROS_H */
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,6 +1,6 @@
 #ifndef FILE_NAME_PRINTER_H
 #define FILE_NAME_PRINTER_H
-
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 void printFileName(void);
 
 #endif /* FILE_NAME_PRINTER_H */
