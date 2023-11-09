fe80a4424ad957a51855242c670bfde5b81848d2
JMI-17 <jmi17.jm@gmail.com>
2023-11-09 09:09:10 -0800

    OXOF T0-T04

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,19 +8,18 @@
 * @cmp: pointer to the function to be used
 *
 * Return: the index of the first element for which the cmp function
-*         does not return 0,
-*      OR -1 if  no element matches or size <= 0.
+* does not return 0,
+* OR -1 if  no element matches or size <= 0.
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i = 0;
-if (size <= 0)
-{
+int i, ret;
+if (array == NULL || size <= 0 || cmp == NULL)
 return (-1);
-}
 for (i = 0; i < size; i++)
 {
-if (cmp(array[i]) != 0)
+ret = cmp(array[i]);
+if (ret != 0)
 {
 return (i);
 }
