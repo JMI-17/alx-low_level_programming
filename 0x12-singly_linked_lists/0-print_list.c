43a755502239140e92a1ace068b6189daeb6c030
JMI-17 <jmi17.jm@gmail.com>
2023-11-16 08:40:31 -0800

    0X12 T0-T6 Linked List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,31 +2,27 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - Prints all the elements of a linked list.
- * @h: A pointer to the head of the linked list.
- *
- * Return: The number of nodes.
- *         If str is NULL, print [0] (nil).
- */
+* print_list - Prints all the elements of a linked list.
+* @h: A pointer to the head of the linked list.
+*
+* Return: The number of nodes. If str is NULL, print [0] (nil).
+*/
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
-	char buffer[20]; /* Adjust the size as needed */
-
-	while (h != NULL)
-	{
-		/* Print the content of the current node */
-		if (h->str == NULL)
-			write(1, "[0] (nil)\n", 11); /* 1 is the file descriptor for stdout */
-		else
-		{
-			int len = sprintf(buffer, "[%d] %s\n", h->len, h->str);
-			write(1, buffer, len);
-		}
-
-		h = h->next;
-		node_count++;
-	}
-
-	return (node_count);
+size_t node_count = 0;
+char buffer[20]; /* Adjust the size as needed */
+while (h != NULL)
+{
+/* Print the content of the current node */
+if (h->str == NULL)
+write(1, "[0] (nil)\n", 11); /* 1 is the file descriptor for stdout */
+else
+{
+int len = sprintf(buffer, "[%d] %s\n", h->len, h->str);
+write(1, buffer, len);
+}
+h = h->next;
+node_count++;
+}
+return (node_count);
 }
